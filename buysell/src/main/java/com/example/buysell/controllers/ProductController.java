59383503cbab3bd855636160ffed2f66bccadf83
Maksim010 <max_dro01@mail.ru>
2024-07-30 19:56:59 +0300

    Security

--- buysell/src/main/java/com/example/buysell/controllers/ProductController.java
+++ buysell/src/main/java/com/example/buysell/controllers/ProductController.java
@@ -5,20 +5,24 @@ import com.example.buysell.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+import java.security.Principal;
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/")
-    public String products(@RequestParam( name = "title",required = false)String title, Model model) {
+    public String products(@RequestParam( name = "title",required = false)String title,Principal principal, Model model) {
         model.addAttribute("products", productService.listProducts(title));
-        return "products";
+        model.addAttribute("user",productService.getUserByPrincipal(principal));
+        return "/products";
     }
 
     @GetMapping("/product/{id}")
@@ -30,11 +34,9 @@ public class ProductController {
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product, @RequestParam("file1")MultipartFile file1 ,@RequestParam("file2")
-    MultipartFile file2,@RequestParam("file3")MultipartFile file3) throws IOException {
-
-
-        productService.saveProduct(product,file1,file2,file3);
+    public String createProduct(Product product, @RequestParam("file1")MultipartFile file1 , @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3")MultipartFile file3, Principal principal) throws IOException {
+        productService.saveProduct(principal,product,file1,file2,file3);
         return "redirect:/";
     }
 
